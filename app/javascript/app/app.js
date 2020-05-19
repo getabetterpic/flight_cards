1f0878b9e156d05e51df1e1070d2ba44bab2b86d
Dan Smith <dan@getabetterpic.com>
2020-05-19 08:40:43 -0400

    Add CSS and JS for styling and removing alerts

--- app/javascript/app/app.js
+++ app/javascript/app/app.js
@@ -1,19 +1,35 @@
 export class App {
   static initApp() {
     document.addEventListener('turbolinks:load', () => {
-      const elems = document.querySelectorAll('.sidenav');
-      const sidenav = window.M.Sidenav.init(elems, {})[0];
+      App.setupSidenav();
+      App.setupAlertClose();
+      M.updateTextFields();
+    });
+  }
 
-      const trigger = document.getElementById('sidenav-trigger');
-      trigger.addEventListener('click', (event) => {
-        event.preventDefault();
-        if (!sidenav) { return; }
-        if (sidenav.isOpen) {
-          sidenav.close();
-        } else {
-          sidenav.open();
+  static setupSidenav() {
+    const elems = document.querySelectorAll('.sidenav');
+    const sidenav = window.M.Sidenav.init(elems, {})[0];
+    const trigger = document.getElementById('sidenav-trigger');
+    trigger.addEventListener('click', (event) => {
+      event.preventDefault();
+      if (!sidenav) { return; }
+      if (sidenav.isOpen) {
+        sidenav.close();
+      } else {
+        sidenav.open();
+      }
+    });
+  }
+
+  static setupAlertClose() {
+    const buttons = document.getElementsByClassName('flash-close');
+    for (let button of buttons) {
+      button.addEventListener('click', (event) => {
+        if (event.target && event.target.parentNode && event.target.parentNode.parentNode) {
+          event.target.parentNode.parentNode.remove();
         }
       });
-    });
+    }
   }
 }
