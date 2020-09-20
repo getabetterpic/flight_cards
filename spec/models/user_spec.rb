a6404c70c7daa0f74cfed4f7d3e6bf9b35579854
Dan Smith <dan@getabetterpic.com>
2020-09-20 07:41:25 -0400

    Add some specs to user and flight card models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,18 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_presence_of(:role) }
+  it { is_expected.to validate_inclusion_of(:role).in_array(%w[flier rso lco launch_admin]) }
+
+  describe '#launch_admin?' do
+    let(:launch_admin) { FactoryBot.build(:user, :launch_admin) }
+    let(:flier) { FactoryBot.build(:user) }
+
+    it 'returns true if role is launch_admin' do
+      expect(launch_admin.launch_admin?).to eq true
+    end
+
+    it 'returns false if role is anything else' do
+      expect(flier.launch_admin?).to eq false
+    end
+  end
 end
